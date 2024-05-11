b04fc4fef5a0e5a7f0ac30fa21b2958c92afd3eb
Alejandro Barba <116202837+alejandroBrbR@users.noreply.github.com>
2024-05-11 04:02:04 +0000

    chore: Refactor patternMatching function and add documentation

--- patternMatching/index.ts
+++ patternMatching/index.ts
@@ -13,6 +13,10 @@ Input: words = ["aa", "bb"], pattern = "zy"
 Output: []
 */
 
+/**
+ * Firs try, deprecated
+ */
+/*
 function patternMatching(words: Array<string>, pattern: string): Array<string> {
   const result: Array<string> = [];
   words.forEach((word) => {
@@ -42,4 +46,53 @@ function patternMatching(words: Array<string>, pattern: string): Array<string> {
   });
   return result;
 }
+*/
+
+/**
+ * Checks if a given word matches a given pattern.
+ * @param {string} word - The word to be checked.
+ * @param {string} pattern - The pattern to match against.
+ * @returns {boolean} - Returns true if the word matches the pattern, otherwise false.
+ */
+function patternMatch(word: string, pattern: string): boolean {
+    // this is a quick check to see if the word is the same length as the pattern
+    if (word.length !== pattern.length) return false;
+
+    const dictionaryLetters: { [key: string]: string } = {}; 
+    const dictionaryPattern: { [key: string]: string } = {}; 
+
+    // Simplify logic and delete flag variable using .every() iterator
+    return word.split("").every((letter, index) => {
+      if (dictionaryLetters[letter]) {
+        if (dictionaryLetters[letter] !== pattern[index]) {
+          return false
+        }
+      } else if (dictionaryPattern[pattern[index]]) {
+        return false
+      }
+
+      // assign the letter to the dictionary
+      dictionaryLetters[letter] = pattern[index];
+      dictionaryPattern[pattern[index]] = letter;
+      return true;
+    });
+}
+
+/**
+ * Filters an array of words based on a pattern.
+ * 
+ * @param words - An array of strings representing words.
+ * @param pattern - A string representing the pattern to match.
+ * @returns An array of strings that match the given pattern.
+ */
+function patternMatching(words: Array<string>, pattern: string): Array<string> {
+  const result: Array<string> = [];
+  words.forEach((word) => {
+    if (patternMatch(word, pattern)) {
+      result.push(word);
+    } 
+  });
+  return result;
+}
+
 export { patternMatching };
